2db2a362b7d63b67a875409a5b25c5db70d1783f
chknwaffles <wonsae.kim@gmail.com>
2019-07-17 21:36:56 -0400

    initial websockets implementation

--- server.js
+++ server.js
@@ -8,17 +8,35 @@ app.use(cors())
 
 const wss = new WebSocket.Server({ port: PORT })
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', (ws) => {
     console.log('Connected!')
-    ws.on('movement', function incoming(data) {
-        //check player movement here
-    })
-    ws.on('message', function incoming(data) {
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState == WebSocket.OPEN) {
+
+    ws.on('message', (data) => {
+        console.log('incoming data', data)
+        console.log('testing data', data.type)
+        console.log('testing data2', data.x)
+
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
                 client.send(data)
-                console.log(data)
+                console.log('sending data', data)
             }
         })
+        switch(data.type) {
+            case 'Bomb': {
+                // startBombTimer()
+                
+                // setTimeout(() => {
+                //     wss.clients.forEach(client => {
+                //         if (client.readyState === WebSocket.OPEN) {
+                //             client.send(data)
+                //             console.log(data)
+                //         }
+                //     })
+                // }, 3000)
+                break;
+            }
+        }
+        
     })
 })
